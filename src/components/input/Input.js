1da252dccecf599c206d42ad482c6b16503b5de8
FacundoVillarroel <facu.villarroel96@gmail.com>
2022-11-12 17:08:23 -0300

    Structure

---
+++ src/components/input/Input.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { styles } from './styles';
+
+const Input = () => {
+  return (
+    <View>
+      <Text>Input</Text>
+    </View>
+  )
+}
+
+export default Input
---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+export default {
+  primary: "#85FFC7",
+  secondary:"#297373",
+  third:"#FF8552",
+  text:"#E6E6E6",
+  background: "#39393A",
+  
+}
